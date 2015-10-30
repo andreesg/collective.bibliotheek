a90e304618f0ff67e29febeed8342efae9e168ab
André Gonçalves <andreslb1@gmail.com>
2015-10-30 17:22:48 +0100

    Do not ask for fb details

--- collective/bibliotheek/utils/views.py
+++ collective/bibliotheek/utils/views.py
@@ -17,76 +17,13 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 # View specific methods #
 # # # # # # # # # # # # #
 
-class BookView(BrowserView):
+class BookView(DefaultView):
     """ View class """
 
     template = ViewPageTemplateFile('../bibliotheek_templates/view.pt')
-
-    """def update(self):
-        super(BookView, self).update()
-        for group in self.groups:
-            if group.__name__ == "title_author":
-                for widget in group.widgets.values():
-                    if IDataGridField.providedBy(widget):
-                        widget.auto_append = False
-                        widget.allow_reorder = True
-                    alsoProvides(widget, IFormWidget)"""
 
     def checkUserPermission(self):
         sm = getSecurityManager()
         if sm.checkPermission(ModifyPortalContent, self.context):
             return True
         return False
-
-    def getFBdetails(self):
-        """item = self.context
-        
-        state = getMultiAdapter(
-                (item, self.request),
-                name=u'plone_context_state')
-
-        # Check view type
-        view_type = state.view_template_id()
-
-        obj = ICanContainMedia(item)
-
-        details = {}
-        details["title"] = item.Title()
-        details["type"] = "article"
-        details["site_name"] = "ZM"
-        details["url"] = item.absolute_url()
-        details["description"] = item.Description()
-        details["double_image"] = ""
-        details["image"] = ""
-        
-        if view_type == "instruments_view":
-            if hasattr(item, 'slideshow'):
-                catalog = getToolByName(self.context, 'portal_catalog')
-                slideshow = item['slideshow']
-                path = '/'.join(slideshow.getPhysicalPath())
-                results = catalog.searchResults(path={'query': path, 'depth': 1, 'portal_type': 'Image'}, sort_on='sortable_title')
-                if len(results) > 0:
-                    lead_image = results[0]
-                    if lead_image.portal_type == "Image":
-                        details["image"] = lead_image.getObject().absolute_url()+"/@@images/image/large"
-                else:
-                    details["image"] = ""
-                
-
-        if details["image"] == "":
-            if obj.hasMedia():
-                image = obj.getLeadMedia()
-                details["image"] = image.absolute_url()+"/@@images/image/large"
-                
-                if view_type == "double_view":
-                    if hasattr(item, 'slideshow'):
-                        slideshow = item['slideshow']
-                        if len(slideshow.objectIds()) > 1:
-                            double_image = slideshow[slideshow.objectIds()[1]]
-                            if double_image.portal_type == "Image":
-                                details["double_image"] = double_image.absolute_url()+"/@@images/image/large"
-            else:
-                details["image"] = ""
-
-        return details"""
-        pass
